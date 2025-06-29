54703616472c043ca943d3c07f99535fe46fb41e
maiko <hm72fuwa@gmail.com>
2025-06-29 13:39:15 +0900

    upDate

--- face.py
+++ face.py
@@ -3,16 +3,12 @@ import time
 from pathlib import Path
 
 import blobconverter
-import cv2
 """
 DepthAIとは深度とAIを組み合わせて使うための見込みプラットフォーム
 OAK-D, OAK-1に対応 AKARIには"OAK-D-LITE"が搭載
 Python, C++対応
 """
 import depthai as dai
-import numpy as np
-from utils.priorbox import PriorBox
-from utils.utils import draw
 
 OPENVINO_VERSION = '2021.4'
 
@@ -25,17 +21,10 @@ class FACE():
             shaves=4
         )) 
 
-    def face_nn(self):
+    def get_face_nn(self):
         return self.face_nn
 
 
-# def face(pipeline, devic, camera) -> bool:
-#     face_nn = pipeline.create(dai.node.NeuralNetwork)
-#     face_nn.setBlobPath(blobconverter.from_zoo(
-#         name="face-detection-retail-0004",
-#         version=OPENVINO_VERSION,
-#         shaves=4
-#     )) 
 
 #     face_nn_xout = pipeline.create(dai.node.XLinkOut)
 #     face_nn_xout.setStreamName("face_nn")
@@ -49,8 +38,3 @@ class FACE():
 #     conf = np.array(face_nn.getLayerFp16("conf")).reshape((1076, 2))
 #     iou = np.array(face_nn.getLayerFp16("iou")).reshape((1076, 1))
 #     loc = np.array(face_nn.getLayerFp16("loc")).reshape((1076, 14))
-
-
-
-
-# # cam.preview.link(face_nn.onput)
